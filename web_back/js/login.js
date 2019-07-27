2bd1b7aafeaedb3527f77f2b09149093fa2d09b8
rotten_fire <zhangjiaweia@126.com>
2019-07-27 11:27:43 +0800

    增加用户登录功能

---
+++ web_back/js/login.js
@@ -0,0 +1,24 @@
+$('#subBtn').click(function () {
+  var inpTxt = $('#inpTxt').val();
+  var inpPwd = $('#inpPwd').val();
+  if(inpTxt == '' || inpPwd == '') {
+    $('#modalMsg').text('用户名或密码不能为空');
+    $('#myModal').modal('show')
+    return;
+  }
+
+  $.ajax({
+    url: 'http://localhost:8000/admin/login',
+    type: 'post',
+    data: {user_name: inpTxt,password: inpPwd},
+    success: function (result) {
+      if(result.code === 200) {
+        window.location.href="index.html";
+      }
+      else if(result.code === 400) {
+        $('#modalMsg').text('用户名或密码错误');
+        $('#myModal').modal('show')
+      }
+    }
+  })
+})
