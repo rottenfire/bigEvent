a4b80032978be5e40142215178a13ca0bd461811
rotten_fire <zhangjiaweia@126.com>
2019-07-29 20:30:03 +0800

    管理员-优化pagination功能

--- web_back/js/article_list.js
+++ web_back/js/article_list.js
@@ -1,36 +1,56 @@
 // 当前删除的条目ID
 var currentDelId = '';
+// 当前页码数
 var currentPage = 1;
+// 当前需筛选的文章类型
 var currentType = '';
+// 当前需筛选的文章状态
 var currentState = '';
+// 当前总page数
+var currentTotalPage = 0;
+// 定义是否为首次生成pagination
+var firstPagination = true;
 
 refreshTr();
 categorySel();
 
-// 文章搜索刷新
+// 调用文章搜索接口，刷新文章列表
 function refreshTr() {
   $.getJSON('http://localhost:8000/admin/search', {
     page: currentPage,
-    type: currentType
+    type: currentType,
+    state: currentState
   }, function (res) {
-    // console.log(res.data);
+    console.log('totalPage:',res.totalPage);
+    // 使用文章列表模板template设置文章列表
     var htmlString = template('articleTr', res);
     $('#artTbody').html(htmlString);
-    $('.pagination').twbsPagination({
-      currentPage: 1, // 初始页
-      totalPages: res.totalPage, // 总页数，可以通过翻页，或者最后一页
-      startPage: 1,
-      visiblePages: 6, // 可见页面
-      first: "首页",
-      last: "尾页",
-      prev: '<',
-      next: '>',
-      onPageClick: function (event, page) {
-        console.log(event, page)
-        currentPage = page;
-        refreshTr();
-      }
-    });
+    // 使用pagination插件
+    if(res.totalPage !== currentTotalPage){
+      currentTotalPage = res.totalPage;
+      $(".pagination").twbsPagination('destroy');
+      firstPagination = true;
+      $('.pagination').twbsPagination({
+        currentPage: 1, // 初始页
+        totalPages: res.totalPage, // 总页数，可以通过翻页，或者最后一页
+        startPage: 1,
+        visiblePages: 6, // 可见页面
+        first: "首页",
+        last: "尾页",
+        prev: '<',
+        next: '>',
+        onPageClick: function (event, page) {
+          console.log(event, page)
+          currentPage = page;
+          // 如果是首次生成pagination则不重复刷新页面
+          if (firstPagination) {
+            firstPagination = false;
+            return;
+          }
+          refreshTr();
+        }
+      });
+    }
   })
 }
 
@@ -64,5 +84,7 @@ $('#modal_del').on('click', '#delBtn', function () {
 $('#btnSearch').on('click',function() {
   currentType = $('#selCategory').val();
   currentState = $('#selStatus').val();
+  console.log(currentType);
+  console.log(currentState);
   refreshTr();
 });
