5a50d6c4d56c65f9cde348e1bbd879ba2bb281b1
rotten_fire <zhangjiaweia@126.com>
2019-07-29 10:14:51 +0800

    管理员-优化删除类别功能

--- web_back/js/article_category.js
+++ web_back/js/article_category.js
@@ -1,5 +1,21 @@
 refreshCategory();
 
+// 给删除按钮绑定事件
+$('#delBtn').on('click', function () {
+  $.post('http://localhost:8000/admin/category_delete', {
+    id: $(this).data('delId')
+  }, function (res) {
+    console.log('res:', res);
+    if (res.code === 200) {
+      console.log('删除成功');
+      // alert(res.msg);
+      refreshCategory();
+    }
+  })
+  $('#modal_del').modal('hide');
+})
+
+
 // 刷新页面
 function refreshCategory() {
   $.get('http://localhost:8000/admin/category_search', function (res) {
@@ -12,14 +28,6 @@ function refreshCategory() {
 
 // 删除按钮绑定事件
 function deleteTr(id) {
-  $.post('http://localhost:8000/admin/category_delete', {
-    id: id
-  }, function (res) {
-    console.log('res:', res);
-    if (res.code === 200) {
-      console.log('删除成功');
-      alert(res.msg);
-      refreshCategory();
-    }
-  })
+  $('#delBtn').data('delId', id);
+  $('#modal_del').modal('show');
 }
